d0fc9f6242f86ea01907e56d9cf8938100d697b8
Alexa Griffin <alexa.griffin42@gmail.com>
2019-05-09 19:54:13 -0400

    basic style compilation

--- index.dev.js
+++ index.dev.js
@@ -1,5 +1,18 @@
 const chokidar = require("chokidar")
+const sass = require('node-sass')
+const fs = require("fs")
 
 chokidar.watch("./site/", {}).on("all", (ev, path) => {
   console.log(ev, path)
+  setTimeout(_ => { // hack to make node-sass work with vscode
+    if(path.indexOf(".sass") !== -1) {
+        sass.render({
+        file: "./site/style/style.sass"
+      }, (err, res) => {
+        if(!err) {
+          fs.writeFile("./out/style/style.css", res.css, () => {})
+        } else console.log(err)
+      })
+    }
+  }, 500)
 })
