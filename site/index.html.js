38aac7412302ef6391ded327f4b233779eddfb71
Alexa Griffin <alexa.griffin42@gmail.com>
2019-05-10 07:22:12 -0400

    basic styles

--- site/index.html.js
+++ site/index.html.js
@@ -1,15 +1,17 @@
 (markupData, pageData) => (
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>{ markupData.name }</title>
-  </head>
-  <body>
-    {
-      markupData.testPages.map(l => <a href={"sub/" + l.fileName + ".html"}>l.fileName</a>)
-    }
-  </body>
-  </html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i|Source+Code+Pro:600" rel="stylesheet" />
+  <link rel="stylesheet" href="style/style.css"/>
+  <title>{ markupData.name }</title>
+</head>
+<body>
+  <div className="hero">
+
+  </div>
+</body>
+</html>
 )
