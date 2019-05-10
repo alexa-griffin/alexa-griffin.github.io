edfed6349302ad900383b9bee8615ed37d2f66bd
Alexa Griffin <alexa.griffin42@gmail.com>
2019-05-09 20:22:25 -0400

    oh good lord this is awful... eh it is about the result right

--- index.dev.js
+++ index.dev.js
@@ -1,6 +1,12 @@
+// file watchers
 const chokidar = require("chokidar")
-const sass = require('node-sass')
 const fs = require("fs")
+
+// compilers
+const babel = require("@babel/core")
+const sass = require('node-sass')
+const React = require('react')
+const ReactDOMServer = require('react-dom/server')
 
 chokidar.watch("./site/", {}).on("all", (ev, path) => {
   console.log(ev, path)
@@ -11,6 +17,19 @@ chokidar.watch("./site/", {}).on("all", (ev, path) => {
       }, (err, res) => {
         if(!err) {
           fs.writeFile("./out/style/style.css", res.css, () => {})
+        } else console.log(err)
+      })
+    } else if(path.indexOf(".html.js") !== -1) {
+      fs.readFile(path, "utf8", (err, data) => {
+        if(!err) {
+          try {
+            fs.writeFile("./out/test.html", "<!DOCTYPE html>" + 
+              ReactDOMServer.renderToString(eval(babel.transform(data, {
+                plugins: ["@babel/plugin-transform-react-jsx"]
+              }).code)), () => { })
+          } catch (error) {
+            console.log(error)
+          }
         } else console.log(err)
       })
     }
