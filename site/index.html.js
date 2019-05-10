dbd06dfe1b69618266bdec7342c38934b109e2da
Alexa Griffin <alexa.griffin42@gmail.com>
2019-05-10 07:51:49 -0400

    hecking dang

--- site/index.html.js
+++ site/index.html.js
@@ -3,15 +3,22 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i|Source+Code+Pro:600" rel="stylesheet" />
   <link rel="stylesheet" href="style/style.css"/>
   <title>{ markupData.name }</title>
 </head>
 <body>
-  <div className="hero">
-
-  </div>
+  <section className="hero">
+    <h1>Hi</h1>
+    <h3>I'm Alexa,</h3>
+    <h4>I make pretty things</h4>
+    <nav>
+      <a href="#">about me</a>
+      <a href="#">my projects</a>
+      <a href="#">get in touch</a>
+    </nav>
+  </section>
 </body>
 </html>
 )
